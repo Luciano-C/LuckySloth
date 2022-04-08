9b926087c16d4d433d57b71329a0efc1e905f2e2
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-04-08 17:40:21 -0400

    hit and love allow now to choose initiator and target

--- commands/hit.js
+++ commands/hit.js
@@ -7,47 +7,96 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('hit')
         .setDescription('Hits a random user.')
-        .addUserOption(option => option.setName('target').setDescription('Select a user to hit instead')),
+        .addUserOption(option => option.setName('target').setDescription('Select someone to hit.'))
+        .addUserOption(option => option.setName('hitter').setDescription('Select someone as hitter.')),
     async execute(interaction) {
         interaction.guild.members.fetch()
             .then(response => {
                 let members = response.map(x => { return { name: x.user.username, isBot: x.user.bot } });
                 let possibleTargets = members.filter(x => x.isBot === false).map(x => x.name);
-                
-                const selectedUser = interaction.options.getUser('target');
-                let hitter = interaction.member.displayName;
-                let reply = "";
+
+                const selectedTarget = interaction.options.getUser('target');
+                const selectedHitter = interaction.options.getUser('hitter');
+
+                let hitter = "";
                 let target = "";
                 let extra = "";
+                let reply = "";
 
 
-                if (selectedUser === null) {
-                    possibleTargets = possibleTargets.filter(x => x !== interaction.member.displayName.toString())
+                if (selectedTarget === null && selectedHitter === null) {
+                    hitter = interaction.member.displayName.toString();
+                    possibleTargets = possibleTargets.filter(x => x !== hitter);
                     target = randomFromArray(possibleTargets);
                     possibleTargets = possibleTargets.filter(x => x !== target);
                     extra = randomFromArray(possibleTargets);
                     reply = hit(hitter, target, extra);
                 }
 
-                else if (selectedUser.username === interaction.member.displayName.toString()) {
-                    reply = "Why are you hitting yourself? :rolling_eyes:";
+                else if (selectedTarget === null && selectedHitter !== null) {
+                    if (selectedHitter.bot === false) {
+                        hitter = selectedHitter.username;
+                        possibleTargets = possibleTargets.filter(x => x !== hitter);
+                        target = randomFromArray(possibleTargets);
+                        possibleTargets = possibleTargets.filter(x => x !== target);
+                        extra = randomFromArray(possibleTargets);
+                        reply = hit(hitter, target, extra);
+                    }
+                    else {
+                        reply = "You can't have bots doing your dirty work, pick another.";
+                    };
                 }
 
-                else if (selectedUser.bot === true) {
-                    hitter = "LuckySloth";
-                    target = interaction.member.displayName.toString();
-                    possibleTargets = possibleTargets.filter(x => x !== target);
-                    extra = randomFromArray(possibleTargets);
-                    reply = `Hitting bots is not cool.\n${hit(hitter, target, extra)}`;
+                else if (selectedTarget !== null && selectedHitter === null && selectedTarget.bot === false) {
+                    if (selectedTarget.bot === false) {
+                        hitter = interaction.member.displayName.toString();
+                        target = selectedTarget.username;
+                        possibleTargets = possibleTargets.filter(x => x !== target);
+                        extra = randomFromArray(possibleTargets);
+                        reply = hit(hitter, target, extra);
+                    }
+                    else {
+                        hitter = "LuckySloth";
+                        target = interaction.member.displayName.toString();
+                        possibleTargets = possibleTargets.filter(x => x !== target);
+                        extra = randomFromArray(possibleTargets);
+                        reply = `Hitting bots is not cool.\n${hit(hitter, target, extra)}`;
+                    }
+
                 }
 
-                else {
-                    target = selectedUser.username;
-                    possibleTargets = possibleTargets.filter(x => x !== target);
-                    extra = randomFromArray(possibleTargets);
-                    reply = hit(hitter, target, extra);
-                }
 
+                else if (selectedTarget !== null && selectedHitter !== null) {
+                    if (selectedTarget.bot === true && selectedHitter.bot === false) {
+                        hitter = "LuckySloth";
+                        target = interaction.member.displayName.toString();
+                        possibleTargets = possibleTargets.filter(x => x !== target);
+                        extra = randomFromArray(possibleTargets);
+                        reply = `Hitting bots is not cool.\n${hit(hitter, target, extra)}`;
+                    }
+
+                    else if (selectedTarget.bot === true && selectedHitter.bot === true) {
+                        reply = "What is it with you and bots? :face_with_raised_eyebrow:";
+                    }
+
+                    else if (selectedTarget.bot === false && selectedHitter.bot === true) {
+                        reply = "You can't have bots doing your dirty work, pick another.";
+                    }
+
+                    else {
+                        hitter = selectedHitter.username;
+                        target = selectedTarget.username;
+                        
+                        if (hitter !== target) {
+                            possibleTargets = possibleTargets.filter(x => x !== target && x !== hitter);
+                            extra = randomFromArray(possibleTargets);
+                            reply = hit(hitter, target, extra);
+                        }
+                        else {
+                            reply = "Weird to see people hitting themselves :thinking:";
+                        };
+                    };
+                };
                 interaction.reply({ content: reply });
             })
             .catch(console.error);
--- commands/love.js
+++ commands/love.js
@@ -3,47 +3,93 @@ const { love } = require("../arrays/loveFunction.js");
 const { randomFromArray } = require('../functions/selectRandomFromArray');
 
 
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('love')
         .setDescription('Loves a random user.')
-        .addUserOption(option => option.setName('target').setDescription('Select a user to love instead')),
+        .addUserOption(option => option.setName('target').setDescription('Select someone to love.'))
+        .addUserOption(option => option.setName('lover').setDescription('Select someone as lover.')),
     async execute(interaction) {
         interaction.guild.members.fetch()
             .then(response => {
                 let members = response.map(x => { return { name: x.user.username, isBot: x.user.bot } });
                 let possibleTargets = members.filter(x => x.isBot === false).map(x => x.name);
-                
-                const selectedUser = interaction.options.getUser('target');
-                let lover = interaction.member.displayName;
-                let reply = "";
+
+                const selectedTarget = interaction.options.getUser('target');
+                const selectedLover = interaction.options.getUser('lover');
+
+                let lover = "";
                 let target = "";
                 let extra = "";
+                let reply = "";
 
 
-                if (selectedUser === null) {
-                    possibleTargets = possibleTargets.filter(x => x !== interaction.member.displayName.toString())
+                if (selectedTarget === null && selectedLover === null) {
+                    lover = interaction.member.displayName.toString();
+                    possibleTargets = possibleTargets.filter(x => x !== lover);
                     target = randomFromArray(possibleTargets);
                     possibleTargets = possibleTargets.filter(x => x !== target);
                     extra = randomFromArray(possibleTargets);
                     reply = love(lover, target, extra);
                 }
 
-                else if (selectedUser.username === interaction.member.displayName.toString()) {
-                    reply = "You know, there are more fun ways to love yourself :wink:";
+                else if (selectedTarget === null && selectedLover !== null) {
+                    if (selectedLover.bot === false) {
+                        lover = selectedLover.username;
+                        possibleTargets = possibleTargets.filter(x => x !== lover);
+                        target = randomFromArray(possibleTargets);
+                        possibleTargets = possibleTargets.filter(x => x !== target);
+                        extra = randomFromArray(possibleTargets);
+                        reply = love(lover, target, extra);
+                    }
+                    else {
+                        reply = "Bots can't love, but they do it as a sex machine.";
+                    };
                 }
 
-                else if (selectedUser.bot === true) {
-                    reply = `Bots may not be able to love, but they do it like a sex machine.`;
+                else if (selectedTarget !== null && selectedLover === null && selectedTarget.bot === false) {
+                    if (selectedTarget.bot === false) {
+                        lover = interaction.member.displayName.toString();
+                        target = selectedTarget.username;
+                        possibleTargets = possibleTargets.filter(x => x !== target);
+                        extra = randomFromArray(possibleTargets);
+                        reply = love(lover, target, extra);
+                    }
+                    else {
+                        reply = "Bots can't love, but they do it as a sex machine.";
+                    }
+
                 }
 
-                else {
-                    target = selectedUser.username;
-                    possibleTargets = possibleTargets.filter(x => x !== target);
-                    extra = randomFromArray(possibleTargets);
-                    reply = love(lover, target, extra);
-                }
 
+                else if (selectedTarget !== null && selectedLover !== null) {
+                    if (selectedTarget.bot === true && selectedLover.bot === false) {
+                        reply = "Bots can't love, but they do it as a sex machine.";
+                    }
+
+                    else if (selectedTarget.bot === true && selectedLover.bot === true) {
+                        reply = "What is it with you and bots? :face_with_raised_eyebrow:";
+                    }
+
+                    else if (selectedTarget.bot === false && selectedLover.bot === true) {
+                        reply = "Bots can't love, but they do it as a sex machine.";
+                    }
+
+                    else {
+                        lover = selectedLover.username;
+                        target = selectedTarget.username;
+                        
+                        if (lover !== target) {
+                            possibleTargets = possibleTargets.filter(x => x !== target && x !== lover);
+                            extra = randomFromArray(possibleTargets);
+                            reply = love(lover, target, extra);
+                        }
+                        else {
+                            reply = "There are more fun ways for people to love themselves :wink:";
+                        };
+                    };
+                };
                 interaction.reply({ content: reply });
             })
             .catch(console.error);
